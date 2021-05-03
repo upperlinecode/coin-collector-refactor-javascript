a51e01af826e2567384187912d4b790ca2014788
Jeff <jolson615@gmail.com>
2021-05-03 17:40:06 -0400

    Update starter.js

--- starter.js
+++ starter.js
@@ -1,29 +1,32 @@
-let coin = {"x": 150, "y": 300}
-let score = 0
-let timeRemaining = 1000
+let coin;
+let score;
+let timeRemaining;
 
 function setup() {
-  createCanvas(400, 400)
+  createCanvas(400, 400);
+  coin = {"x": 150, "y": 300};
+  timeRemaining = 1000;
+  score = 0;
 }
 
 function draw() {
-  background(230)
-  ellipse(coin.x, coin.y, 20, 20)
-  ellipse(mouseX, mouseY, 20, 20)
+  background(230);
+  ellipse(coin.x, coin.y, 20, 20);
+  ellipse(mouseX, mouseY, 20, 20);
   
   if (collideCircleCircle(coin.x, coin.y, 20, mouseX, mouseY, 20) && timeRemaining > 0) {
-    coin.x = random(width)
-    coin.y = random(height)
-    score++
+    coin.x = random(width);
+    coin.y = random(height);
+    score++;
   }
-  text("You're score is " + score, 10, 20)
+  text("You're score is " + score, 10, 20);
   
   if (timeRemaining > 0) {
-    timeRemaining--
+    timeRemaining--;
   }
-  text("Time remaining: " + timeRemaining, 10, 40)
+  text("Time remaining: " + timeRemaining, 10, 40);
   
   if (timeRemaining === 0) {
-    text("Game over", 10, 60)
+    text("Game over", 10, 60);
   }
 }
